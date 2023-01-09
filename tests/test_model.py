7e92cf808fc489143df8f54119afc3e8a6ba7a31
s185231 <s185231@student.dtu.dk>
2023-01-09 16:36:21 +0100

    Data

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,10 @@
 from src.data.data import mnist
 from src.models.model import MyAwesomeModel
 
-
-
 def test_model(idx):
     train, test = mnist()
     im = test[0][idx]
     num_classes = 5
     model = MyAwesomeModel(num_classes)
     out = model(im)
-    assert test_model(0) == 5
+    assert out
